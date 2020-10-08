fe306106d09313601f53ddb948eb94e9dda4e866
Martin <myspammbox@seznam.cz>
2020-10-08 14:03:49 +0200

    LoginFragment + ViewModel added

--- app/src/main/java/opkp/solutions/bookingapp/fragments/LoginFragment.kt
+++ app/src/main/java/opkp/solutions/bookingapp/fragments/LoginFragment.kt
@@ -1,16 +1,16 @@
 package opkp.solutions.bookingapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import opkp.solutions.bookingapp.R
+import opkp.solutions.bookingapp.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,16 +18,14 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var mAuth: FirebaseAuth? = null
+    private var currentUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+
     }
 
     override fun onCreateView(
@@ -35,26 +33,33 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+//        return inflater.inflate(R.layout.fragment_login, container, false)
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false
+        )
+
+        mAuth = FirebaseAuth.getInstance()
+        currentUser = mAuth?.currentUser
+
+        binding.loginButton.setOnClickListener {
+            //TODO validate database account and navigate to CalendarFragment
+        }
+
+        binding.createAccountButton.setOnClickListener{
+            //TODO navigate to CreateAccountFragment
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = mAuth?.currentUser
+
     }
 }
---
+++ app/src/main/java/opkp/solutions/bookingapp/viewmodels/LoginViewModel.kt
@@ -0,0 +1,6 @@
+package opkp.solutions.bookingapp.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+}
